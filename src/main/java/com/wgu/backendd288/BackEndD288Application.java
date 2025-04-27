e679c5165b71961a136ed48034ecc3dbdda980c0
tdao15 <tdao15@wgu.edu>
2025-04-26 21:38:19 -0700

    Part B: setup lab environment and git

---
+++ src/main/java/com/wgu/backendd288/BackEndD288Application.java
@@ -0,0 +1,13 @@
+package com.wgu.backendd288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndD288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndD288Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wgu/backendd288/BackEndD288ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgu.backendd288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndD288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
