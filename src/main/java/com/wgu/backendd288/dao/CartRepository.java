7cf4652c56c1a457dd9da74f2b1b7543e47c7788
tdao15 <tdao15@wgu.edu>
2025-04-29 21:59:07 -0700

    Part E: Added DAO repositories for each entity with cross origin.

---
+++ src/main/java/com/wgu/backendd288/dao/CartRepository.java
@@ -0,0 +1,9 @@
+package com.wgu.backendd288.dao;
+
+import com.wgu.backendd288.entities.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
